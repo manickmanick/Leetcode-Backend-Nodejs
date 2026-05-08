5f8c1540aaca68eb78a07e8d9636bcfb61dee8b9
manickmanick <manickaperumal22@gmail.com>
2026-05-08 19:18:20 +0530

    db config and redis configuration initialization was done

---
+++ SubmissionService/src/config/redis.config.ts
@@ -0,0 +1,28 @@
+import Redis from "ioredis";
+import logger from "./logger.config";
+
+const redisConfig = {
+    host: process.env.REDIS_HOST || "localhost",
+    port: Number(process.env.REDIS_PORT) || 6379,
+    maxRetriesPerRequest: null,
+    retryStrategy: (times: number) => {
+        if(times > 3) {
+            return null;
+        }
+        return Math.min(times * 100, 3000); // 3 seconds
+    }
+}
+
+export const redis = new Redis(redisConfig);
+
+redis.on("connect", () => {
+    logger.info("Connected to redis successfully");
+});
+
+redis.on("error", (error) => {
+    logger.error("Redis connection error", error);
+});
+
+export const createNewRedisConnection = () => {
+    return new Redis(redisConfig);
+}
